1091681ac323107bc49466e907779df11471329d
Marshall B <mbolen13fox@gmail.com>
2022-05-03 22:20:49 -0500

    added sequencer, stop/play

--- index.js
+++ index.js
@@ -1,12 +1,85 @@
+
+
+const SeqRow = (props) => {
+
+    const addClass = (e) => {
+        e.classList.length < 3 ? e.classList.add('seq-btn-active') : e.classList.remove('seq-btn-active') 
+    }
+
+    return (
+      <div className='seq-row'>
+          <div className='seq-btn col-1' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-2' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-3' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-4' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-5' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-6' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-7' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-8' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-9' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-10' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-11' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-12' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-13' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-14' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-15' onClick={(e)=>addClass(e.target)}></div>
+          <div className='seq-btn col-16' onClick={(e)=>addClass(e.target)}></div>
+      </div>
+    )
+}
+
 const App = () => {
 
     const [displayText, setDisplayText] = React.useState('')
+    const [timerIsActive, setTimerIsActive] = React.useState(false)
+    const [bpm, setBpm] = React.useState(100)
+    const [step, setStep] = React.useState(1)
 
     React.useEffect(() => {
         document.addEventListener('keydown', (event) => {
             playAudio(event.key.toUpperCase())
         })
     }, [])
+
+    const playStep = (num) => {
+        const seqStep = document.getElementsByClassName(`col-${num}`)
+
+    }
+
+    let bps = bpm / 60
+    let counter = 1
+    let intervalId
+
+    React.useEffect(() => {
+        if (timerIsActive) {
+            intervalId = setInterval(()=> {
+                console.log('tick')
+               
+                if (counter < 16){
+                    counter++
+                    setStep(counter)
+                } else {
+                    counter = 1
+                    setStep(counter)
+                }
+
+                if (counter === 1) {
+
+                }
+            }, 1000 / bps) 
+        } 
+        return function clear() {
+            clearInterval(intervalId)
+            console.log('return clear')
+        }
+        
+    }, [timerIsActive])
+
+    const timer = () => {
+        counter = 1
+        setStep(1)
+        timerIsActive ? setTimerIsActive(false) : setTimerIsActive(true)
+    }
 
     const playAudio = (letter) => {
         const audio = document.getElementById(letter)
@@ -77,21 +150,33 @@ const App = () => {
         return <div onClick={() => {playAudio(e[0])}} key={e[0]} className='drum-pad' id={e[1]}>{e[0]}<audio className="clip" id={e[0]} src={e[2]}></audio></div>
     })
 
+    const clicked = () => {
+        console.log('clicked')
+    }
+
   return (
-    <div className='container overall'>
-        
-        <div id='drum-machine' >
-            <div id='title-box'>
-                <h1 id="title">Beatboi 9000</h1>
+    <div id='container'>
+        <div className='row'>
+            <div id='drum-machine' >
+                <div id='title-box'>
+                    <h1 id="title">Beatboi 9000</h1>
+                </div>
+                <div id='drum-pads'>
+                    {padKeys}
+                </div>
+                <div id='display-box'>
+                    <div id='display'><p>{displayText}</p></div>
+                    <div id='display'><p>{step}</p></div>
+                    <button onClick={timer}>Play/Pause</button>
+                </div>
             </div>
-            <div id='drum-pads'>
-                {padKeys}
-            </div>
-            <div id='display-box'>
-                <div id='display'><p>{displayText}</p></div>
+            <div id='sequencer'>
+                <SeqRow clicked={clicked}/>
+                <SeqRow clicked={clicked}/>
+                <SeqRow clicked={clicked}/>
+                <SeqRow clicked={clicked}/>
             </div>
         </div>
-        
     </div>
   )
 }
